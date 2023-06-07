b769be046b8887f2f25b7c7d13858b7f52e80e29
Przemysław Soliński <solinskip107@gmail.com>
2023-06-07 20:49:05 +0200

    app.jsx structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
+import user from './Profile/user.json';
+import { Profile } from './Profile/Profile';
+import data from './Statistics/data.json';
+import { Statistics } from './Statistics/Statistics';
+import friends from './FriendList/friends.json';
 import { FriendList } from './FriendList/FriendList';
+import transactions from './TransactionHistory/transactions.json';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 export const App = () => {
   return (
